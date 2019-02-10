3e889b100d09c457ff86221acbd9cb7abd0b8d48
bashkos <bashkos@gmail.com>
2019-02-10 21:40:54 +0900

    Inital state for dashboard with Search component

--- src/pages/dashboard/reducers.js
+++ src/pages/dashboard/reducers.js
@@ -1,6 +1,9 @@
 import { UPDATE_WINDOWS } from './actions';
+import { WIDGET_SEARCH } from './widgets';
 
-const dashboard = (state = null, action) => {
+const initialState = WIDGET_SEARCH;
+
+const dashboard = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_WINDOWS:
       return action.dashboard;
