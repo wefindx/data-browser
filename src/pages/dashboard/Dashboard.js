a9dc6fb3981c2b211da06b745d87f061188e2142
bashkos <bashkos@gmail.com>
2019-02-26 20:33:03 +0900

    Replace dashboard navbar with context menu and add thin theme for mosaic

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,26 +1,56 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { Mosaic } from 'react-mosaic-component';
-import DashboardNavbar from './DashboardNavbar';
+import { Menu, Submenu, Item, IconFont, MenuProvider } from 'react-contexify';
+import { Icon } from '@blueprintjs/core';
 import { widgets } from './widgets';
 import { updateWindows } from './actions';
-import './Dashboard.css';
+import { widgetIsOpened, openWidget } from './utils';
+
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import 'react-mosaic-component/react-mosaic-component.css';
+import 'react-contexify/dist/ReactContexify.min.css';
+import './css/mosaic-thin-theme.css';
+import './css/dashboard.css';
+import './css/menu.css';
 
 class Dashboard extends PureComponent {
+  widgetsMenu = (
+    <Menu id="widgets-menu" className="menu">
+      <Submenu label="Open widget...">
+        {Object.keys(widgets).map(widget => (
+          <Item
+            className="menu_item"
+            key={widget}
+            onClick={() => this.onSelectWidget(widget)}
+          >
+            <Icon icon={widgets[widget].icon} className="menu_icon" />
+            {widgets[widget].title}
+          </Item>
+        ))}
+      </Submenu>
+    </Menu>
+  );
+
+  onSelectWidget(widget) {
+    if (!widgetIsOpened(widget, this.props.dashboard)) {
+      this.props.updateWindows(openWidget(widget, this.props.dashboard));
+    }
+  }
+
   render() {
     return (
       <div className="dashboard">
-        <DashboardNavbar></DashboardNavbar>
-        <div className="dashboard_content">
+        <MenuProvider id="widgets-menu" className="dashboard_content">
           <Mosaic
+            className="mosaic-blueprint-theme mosaic-thin-theme"
             renderTile={(id, path) => widgets[id].component(path, this.props, this.state)}
             value={this.props.dashboard}
             onChange={this.props.updateWindows}
           ></Mosaic>
-        </div>
+        </MenuProvider>
+        {this.widgetsMenu}
       </div>
     );
   }
